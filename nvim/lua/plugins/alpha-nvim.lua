109f77661edff04540d5f3c1daeaee72afc20ade
siso25 <siso25@zoho.com>
2024-06-03 01:04:47 +0900

    Neovimのダッシュボードの見た目を修正

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -2,7 +2,6 @@ return {
   'goolord/alpha-nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function ()
-    -- require'alpha'.setup(require'alpha.themes.dashboard'.config)
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
 
@@ -24,6 +23,18 @@ return {
         dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
         dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
         dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+    }
+
+    dashboard.section.header.opts.hl = "DashboardHeader"
+    dashboard.section.footer.opts.hl = "DashboardFooter"
+
+    dashboard.config.layout = {
+      { type = "padding", val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) } },
+      dashboard.section.header,
+      { type = "padding", val = 5 },
+      dashboard.section.buttons,
+      { type = "padding", val = 3 },
+      dashboard.section.footer,
     }
 
     -- Send config to alpha
