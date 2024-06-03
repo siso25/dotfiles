660e214e1c526efe9dfdf92e54dc19953e31c1f0
siso25 <siso25@zoho.com>
2024-06-04 01:00:31 +0900

    nvim-cmpとtelescopeのキーマップを追加

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,6 +3,17 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<Leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<Leader>ff", function() builtin.find_files({ hidden = false }) end, {})
+    vim.keymap.set("n", "<Leader>fF", function() builtin.find_files({ hidden = true }) end, {})
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = { require("telescope.actions").move_selection_next, type = "action", opts = { nowait = true, silent = true } },
+            ["<C-k>"] = { require("telescope.actions").move_selection_previous, type = "action", opts = { nowait = true, silent = true } },
+          }
+        }
+      }
+    })
   end
 }
