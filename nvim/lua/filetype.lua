86fd63569ba1d1db8ac70ce55c875125c5540b67
siso25 <siso25@zoho.com>
2024-09-08 16:27:15 +0900

    不要な判定を修正
    is_hard_tabの値を反転させるような処理になっていて少し読みづらいので、
    is_soft_tabとして、値をそのまま設定するように修正した。

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -1,12 +1,7 @@
 local M = {}
 
-local function set_indent(tab_length, is_hard_tab)
-  if is_hard_tab then
-    vim.bo.expandtab = false
-  else
-    vim.bo.expandtab = true
-  end
-
+local function set_indent(tab_length, is_soft_tab)
+  vim.bo.expandtab = is_soft_tab
   vim.bo.shiftwidth = tab_length
   vim.bo.softtabstop = tab_length
   vim.bo.tabstop = tab_length
@@ -17,17 +12,17 @@ M.help = function()
 end
 
 M.markdown = function()
-  set_indent(4, false)
+  set_indent(4, true)
 end
 
 M.text = function()
-  set_indent(4, true)
+  set_indent(4, false)
 end
 
 return setmetatable(M, {
   __index = function()
     return function()
-      set_indent(2, false)
+      set_indent(2, true)
     end
   end
 })
