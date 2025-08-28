39557fe650255aa82543d13bd0a48a2e29a040e4
siso25 <shota.shibano@luxiar.com>
2025-08-28 18:43:36 +0900

    プラグイン管理をmini.nvimに移行

--- nvim/lua/plugins/vim-easymotion.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "easymotion/vim-easymotion",
-  config = function()
-    vim.g.EasyMotion_use_migemo = 1
-    vim.keymap.set("n", "f", "<Plug>(easymotion-f)", {})
-    vim.keymap.set("n", "F", "<Plug>(easymotion-F)", {})
-  end
-}
